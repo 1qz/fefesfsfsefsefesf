4af1cbe1c280630b726abaaf6f27bd47a62caa21
1qz <36353192+1qz@users.noreply.github.com>
2018-09-27 16:19:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2191,18 +2191,6 @@ client.on('message', message => {
       message.author.sendEmbed(Embed11)
     }
 });
-client.on("guildMemberAdd", function(member) {
-    const wc = member.guild.channels.find("name", "general")
-        const embed = new Discord.RichEmbed()
-        .setColor('B90C0C')
-        .setAuthor(member.user.tag, member.user.avatarURL)
- .setDescription('***يا مرحبا وسهلاً بضيف لفانا، يزهي بك الأدب العربي وينثر لك أزهار يسقيك من نبع المشاعر وفانا، لين الهلا تثمر على غصونك أطيار. ***')
-.setThumbnail(member.avatarURL)
-  .setImage('https://cdn.pg.sa/i2AboP4evX.png')
-        .setTimestamp()
-        return wc.sendEmbed(embed);
-        
-});
 client.on('message', message => {
     if (message.content.startsWith("-avatar")) {
         if (message.author.bot) return
@@ -2227,6 +2215,56 @@ client.on('message', message => {
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","مواطن"));
     });
+const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
+const coolDown = new Set();
+
+client.on('message',async message => {
+    
+if(message.author.bot) return;
+if(!credits[message.author.id]) credits[message.author.id] = {
+    credits: 50
+};
+
+let userData = credits[message.author.id];
+let m = userData.credits;
+
+fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
+    if (err) console.error(err);
+  });
+  credits[message.author.id] = {
+      credits: m + 0.5,
+  }
+  
+    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
+message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
+}
+});
+
+client.on('message', async message => {
+    let amount = 250;
+    if(message.content.startsWith(prefix + "daily")) {
+    if(message.author.bot) return;
+    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
+    
+    let userData = credits[message.author.id];
+    let m = userData.credits + amount;
+    credits[message.author.id] = {
+    credits: m
+    };
+
+    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
+    if (err) console.error(err);
+    });
+    
+    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
+        coolDown.add(message.author.id);
+    });
+    
+    setTimeout(() => {
+       coolDown.remove(message.author.id);
+    },86400000);
+    }
+});
 
 
 client.login(process.env.BOT_TOKEN);
