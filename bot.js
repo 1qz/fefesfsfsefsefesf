04eb6da3625fec20d85af38c11117105aee741fa
1qz <36353192+1qz@users.noreply.github.com>
2018-10-02 05:38:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,12 +31,12 @@ message.author.sendEmbed(embed)
 }); 
 client.on('message', msg => {
 if (msg.content.includes('IP')) {
-msg.reply(`92.222.205.219:30120`)
+msg.reply(`92.222.205.219:30170`)
 }
 });
 client.on('message', msg => {
 if (msg.content.includes('ip')) {
-msg.reply(`92.222.205.219:30120`)
+msg.reply(`92.222.205.219:30170`)
 }
 });
  client.on('guildCreate', (guild) => {
