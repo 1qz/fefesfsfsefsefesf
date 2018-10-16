9c56c8477d4e3929aac22ba5755a114fa14ec456
1qz <36353192+1qz@users.noreply.github.com>
2018-10-16 17:55:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2409,7 +2409,7 @@ const w = ['./welcome.png'];
                         ctx.fontSize = '20px';
                         ctx.fillStyle = "#f1f1f1";
                         ctx.textAlign = "center";
-                        ctx.fillText(member.user.username, 180, 500);
+                        ctx.fillText(member.user.username, 350, 110);
  
                 let Avatar = Canvas.Image;
                               let ava = new Avatar;
