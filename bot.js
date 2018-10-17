6616bd1ab7f41422cc5f957f8a9c1d75e4a361de
1qz <36353192+1qz@users.noreply.github.com>
2018-10-17 23:42:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2415,10 +2415,10 @@ const w = ['./welcome.png'];
                               let ava = new Avatar;
                               ava.src = buf;
                               ctx.beginPath();
-                              ctx.arc(8, 45, 100, 0, Math.PI*2);
+                              ctx.arc(100, 101, 100, 0, Math.PI*2);
                               ctx.stroke();
                                  ctx.clip();
-                                 ctx.drawImage(ava, 13, 38, 251, 251);  
+                                 ctx.drawImage(ava, 13, 38, 470, 180);  
                           
                 
                              
