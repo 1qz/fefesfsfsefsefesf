d2eeaac62133a6d44414cee8811c4684dcf93e80
1qz <36353192+1qz@users.noreply.github.com>
2018-11-18 15:27:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,12 +31,12 @@ message.author.sendEmbed(embed)
 }); 
 client.on('message', msg => {
 if (msg.content.includes('IP')) {
-msg.reply(`**92.222.205.219:30170**`)
+msg.reply(`**79.137.44.18:30170**`)
 }
 });
 client.on('message', msg => {
 if (msg.content.includes('ip')) {
-msg.reply(`92.222.205.219:30170`)
+msg.reply(`79.137.44.18:30170`)
 }
 });
  client.on('guildCreate', (guild) => {
