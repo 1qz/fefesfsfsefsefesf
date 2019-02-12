c91eabb5111f87e3d7f58b41c9c7c784d1f7b37a
1qz <36353192+1qz@users.noreply.github.com>
2019-02-12 13:58:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ msg.reply(159.100.6.216:30120)
 });
 client.on('message', msg => {
 if (msg.content.includes('ip')) {
-msg.reply(51.38.127.104:30120)
+msg.reply(159.100.6.216:30120)
 }
 });
  client.on('guildCreate', (guild) => {
