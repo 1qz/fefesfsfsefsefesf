a4b47a85568689575e3e9b0ec5488445fa4f6f7b
1qz <36353192+1qz@users.noreply.github.com>
2019-02-12 13:31:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2601,7 +2601,7 @@ client.on('message', message => {
                              })
  }
  });
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN2);
 client.on("message", async message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
