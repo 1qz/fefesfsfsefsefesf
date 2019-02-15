ea2ee6f4218eab9a050001835dbc5eda8339c1cf
1qz <36353192+1qz@users.noreply.github.com>
2019-02-15 21:04:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,12 +31,12 @@ message.author.sendEmbed(embed)
 });
 client.on('message', msg => {
 if (msg.content.includes('IP')) {
-msg.reply(`الشباب شغالين صبرك ياذيب`)
+msg.reply(`159.100.6.216:30121`)
 }
 });
 client.on('message', msg => {
 if (msg.content.includes('ip')) {
-msg.reply(`الشباب شغالين صبرك ياذيب`)
+msg.reply(`159.100.6.216:30121`)
 }
 });
  
